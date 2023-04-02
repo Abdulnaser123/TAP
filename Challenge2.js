c93f2cb8b5c69fcc1977499b986a302db2083af0
Abdulnasser M.Obeid <108693961+Abdulnaser123@users.noreply.github.com>
2023-04-02 04:05:24 +0300

    Update Challenge2.js

--- Challenge2.js
+++ Challenge2.js
@@ -12,3 +12,16 @@ function missingNumber(integers) {
   }
   return x;
 }
+
+
+//another solution, the idea of these two solutions is to find the exepted sum from the array and subtracte actual sum from it to find the miss num
+
+function findMissingNumber(nums) {
+  const n = nums.length;
+  const expectedSum = (n * (n + 1)) / 2; 
+  const actualSum = nums.reduce((sum, num) => sum + num); 
+  return expectedSum - actualSum;
+}
+console.log(findMissingNumber([3, 0, 1])); 
+console.log(findMissingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1])); 
+console.log(findMissingNumber([0, 1])); 
